6e6e8cdd6714aa53911fe3e0d731768d05a83fdf
Patrick Silva <martins.patrick3@gmail.com>
2024-04-13 17:43:42 -0300

    feat: add series list on home page

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,13 +1,52 @@
+import { NgFor } from '@angular/common';
 import { Component } from '@angular/core';
-import { IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/angular/standalone';
+import {
+  IonHeader,
+  IonToolbar,
+  IonTitle,
+  IonContent,
+  IonList,
+  IonItem,
+  IonLabel,
+  IonToast,
+  IonButton,
+  ToastController,
+} from '@ionic/angular/standalone';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
   standalone: true,
-  imports: [IonHeader, IonToolbar, IonTitle, IonContent],
+  imports: [
+    IonHeader,
+    IonToolbar,
+    IonTitle,
+    IonContent,
+    IonList,
+    IonItem,
+    NgFor,
+    IonLabel,
+    IonToast,
+    IonButton,
+  ],
 })
 export class HomePage {
-  constructor() {}
+  seriesList = [
+    { id: 1, title: 'Supernatural' },
+    { id: 2, title: 'Dr. House' },
+    { id: 3, title: 'How I Meet Your Mother' },
+    { id: 4, title: 'The Big Bang Theory' },
+    { id: 5, title: 'Breaking Bad' },
+  ];
+
+  constructor(private toastController: ToastController) {}
+
+  async showClickedItemName(itemName: string) {
+    const toast = await this.toastController.create({
+      message: `Você clicou na série ${itemName}`,
+    });
+
+    toast.present();
+  }
 }
